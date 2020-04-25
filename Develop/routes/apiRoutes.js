e59dbf11f4390df3463794f1fb00de9122e68a0e
Philip Hurst <youngphil9@gmail.com>
2020-04-25 13:35:28 -0500

    applied get, add, and delete notes api, linked to server.js, blank db.json

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,16 +1,58 @@
-const path = require('path');
 const fs = require('fs');
 
 module.exports = function(app) {
     app.get('/api/notes', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/notes.html'));
+        read(res);
     });
 
     app.post('/api/notes', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        addNote(req);
+        read(res);
     });
 
     app.delete('/api/notes/:id', function(req, res) {
-        res.sendFile(path.join(__dirname, '../public/index.html'));
+        deleteNote(req);
+        read(res);
+    });
+}
+
+function read(res) {
+        fs.readFile('./db/db.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        const dataJSON = JSON.parse(data);
+        res.json(dataJSON);
+    })
+}
+
+function addNote(req) {
+        fs.readFile('./db/db.json', 'utf8', function(err, data) {
+            if (err) throw err;
+
+            const dataJSON = JSON.parse(data);
+            const newNotes = [...dataJSON, req.body];
+            newNotes.map(obj => {
+                obj.id = Math.random();
+            })
+            writeDb(newNotes);
+        });
+}
+
+function writeDb(newNotes) {
+    fs.writeFile('./db/db.json', JSON.stringify(newNotes, null, 2), function(err) {
+        if (err) throw err;
+    });
+}
+
+function deleteNote(req) {
+    fs.readFile('./db/db.json', 'utf8', function(err, data) {
+        if (err) throw err;
+        const dataJSON = JSON.parse(data);
+        const newArray = [];
+        dataJSON.map(obj => {
+            if (obj.id != req.params.id) {
+                newArray.push(obj);
+            }; 
+        });
+        writeDb(newArray);
     });
 }
